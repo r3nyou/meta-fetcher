65249c19d76ee10368483a2c32cf588082194eb4
Jian Marcus <renyou815@gmail.com>
2023-11-12 12:02:26 +0800

    feat(comments): fetch page feeds

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<template></template>
+
+<script>
+// TODO env
+// TODO separate component
+// TODO store token in memory
+// TODO store token in firebase
+const facebookAppId = 246637738109326
+const pageId = 171613189363559
+
+const FB = await getFbSdk()
+FB.api(
+  `/${pageId}/feed`,
+  'get',
+  { 'access_token': 'EAADgUNLEqY4BOZBfZBZA9WmUrr8geY3ZBBQ6zhBAsdO0PV18auyYaZBEl8d77hDw6MmRqboKNJH8TzatUXYeJ19HU785JnoxqRpyC6n9tDkzHNJLt4naPpeeECbQDPyzMab9ZBWgyWa6Y03GpE53vOHTAwqJxCSKvG1eWIetewwe9WRxSJZBq0olY3h9iP5qqkHfpyNoSbc1ZC7sClp9719RKSLHyULXnEdOKFB6DvIZD' },
+  function (response) {
+    if (response && !response.error) {
+      console.log(response)
+    } else {
+      console.log('error', response.error)
+    }
+  }
+);
+
+function initFacebookSdk() {
+  return new Promise(resolve => {
+    // wait for facebook sdk to initialize before starting the vue app
+    window.fbAsyncInit = function () {
+      const FB = window.FB;
+      FB.init({
+        appId: facebookAppId,
+        cookie: true,
+        xfbml: true,
+        version: 'v18.0'
+      });
+      resolve();
+    };
+
+    // load facebook sdk script
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  });
+}
+
+function getFbSdk() {
+  return new Promise(async resolve => {
+    if (window.FB) {
+      resolve(window.FB)
+    } else {
+      await initFacebookSdk()
+      resolve(window.FB)
+    }
+  })
+}
 </script>
-
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
